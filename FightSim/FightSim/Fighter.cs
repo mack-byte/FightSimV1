797f008fadc1f2badcf49737cf103665ab282d0c
mack-byte <55237311+mack-byte@users.noreply.github.com>
2019-10-10 14:09:11 +0200

    Start

---
+++ FightSim/FightSim/Fighter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSim
+{
+    class Fighter
+    {
+        int hp = 100;
+        
+        public string name;
+        
+
+
+        Random generator = new Random();
+
+        public void Attack()
+        {
+            int r = generator.Next(50);
+        }
+        public int Hurt1(int r)
+        {
+            hp -= r;
+            return r;
+        }
+        
+        public bool Alive()
+        {
+            if (hp < 0)
+            {
+                return false;
+            }
+            
+            return true;
+        }
+        public int GetHp()
+        {
+            return hp;
+        }
+        
+        
+       
+
+    }
+
+ }
---
+++ FightSim/FightSim/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FightSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fighter f1 = new Fighter();
+            Fighter f2 = new Fighter();
+            Console.WriteLine("Name fighter 1");
+            f1.name = Console.ReadLine();
+            Console.WriteLine("Name fighter 2");
+            f2.name = Console.ReadLine();
+            Console.WriteLine(f1.name + f2.name);
+            while (f1.Alive() == true)
+            {
+                
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
