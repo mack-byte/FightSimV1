a049ff9c2ea2e77c5e9604c153f5f7ed5fab827d
mack-byte <55237311+mack-byte@users.noreply.github.com>
2019-10-10 14:43:41 +0200

    V1 Final

--- FightSim/FightSim/Fighter.cs
+++ FightSim/FightSim/Fighter.cs
@@ -3,39 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace FightSim
 {
     class Fighter
     {
-        int hp = 100;
+        int hp = 100; //variabel för hur mycket hp båda börjar med
         
-        public string name;
+        public string name; //string för att lagra de namn som spelaren skriver in
         
 
+        
+        Random generator = new Random(); //Slumpar hur mycket skada man gör
+        Random generator2 = new Random();
 
-        Random generator = new Random();
-
-        public void Attack()
+        public int Attack() //Metod för attacken där båda spelarna kan göra mellan 0 och 50 skada
         {
-            int r = generator.Next(50);
+            
+            int amount = generator.Next(50);
+            return amount;
         }
-        public int Hurt1(int r)
+        public int Attack2()
         {
-            hp -= r;
-            return r;
+
+            int amount = generator2.Next(50);
+            return amount;
+        }
+        public void Hurt1(int amount) //Metod för att dra av antalet ska från hp
+        {
+           
+            hp -= amount;
+            
         }
         
-        public bool Alive()
+        public bool Alive() //Boolvärde för att avgöra ifall spelaren är vid liv
         {
-            if (hp < 0)
+            if (hp < 1)
             {
-                return false;
+                return false;// ifall hp är under 1 så kommer den returnera false
+            }
+            else
+            {
+                return true;
             }
             
-            return true;
         }
-        public int GetHp()
+        public int GetHp() //Metod för att returnera den privata inten
         {
             return hp;
         }
--- FightSim/FightSim/Program.cs
+++ FightSim/FightSim/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace FightSim
 {
@@ -10,16 +11,36 @@ namespace FightSim
     {
         static void Main(string[] args)
         {
-            Fighter f1 = new Fighter();
+            Fighter f1 = new Fighter();//Skapar en kämpe
             Fighter f2 = new Fighter();
             Console.WriteLine("Name fighter 1");
-            f1.name = Console.ReadLine();
+            f1.name = Console.ReadLine();//Lagrar spelarens input i name variablen
             Console.WriteLine("Name fighter 2");
             f2.name = Console.ReadLine();
-            Console.WriteLine(f1.name + f2.name);
-            while (f1.Alive() == true)
+            
+            while (f2.Alive() == true && f1.Alive() == true) //Ifall båda spelarna är vid liv så körs loopen
             {
+
                 
+                f1.Attack();//Kallar metoden för attack
+                
+                f2.Hurt1(f1.Attack());//Använder attack som parameter för hur mycket hp som ska dras av 
+                Thread.Sleep(1000);//Skapar mellanrum mellan varje slag
+                Console.WriteLine(f2.name + " has " + f2.GetHp() + " HP");//skriver ut hur mycket hp spelarna har
+                f2.Attack();
+                
+                f1.Hurt1(f2.Attack2());
+                Console.WriteLine(f1.name + " has " + f1.GetHp() + " HP");
+                
+                
+            }
+            if (f2.Alive() == false)
+            {
+                Console.WriteLine("Congrats " + f1.name);
+            }
+            if (f1.Alive() == false)
+            {
+                Console.WriteLine("Congrats " + f2.name);
             }
 
             Console.ReadLine();
